88fe7d7ca17fc8ebdf76bee177f996804a2bbd8c
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-18 22:12:53 +0000

    calculator init

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 // Copyright (c) 2022 liya getachew All rights reserved
 //
 // Created by: liya getachew
-// Created on: Oct 2022
+// Created on: Oct 18 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 /**
- * This function calculates area of a triangle.
+ * This function calculates the pay of the user.
  */
+function calculatePay() {
+  // input
+  const hours = parseFloat(document.getElementById('hours').value)
+  const salary = parseFloat(document.getElementById('salary').value)
+   const TAX_RATE = 0.18
+  
+  // process
+  const takeHomeSalary = (hours * salary) * (1.00 - TAX_RATE)
+  const taxes = (hours * salary) * TAX_RATE
+  
+  // output
+  document.getElementById("pay").innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+   document.getElementById("taxed").innerHTML = `The government will take: $ ${taxes.toFixed(2)}`
+}
+
